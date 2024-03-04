8a079c141a9d435700e6c1c2f1abe394577967ff
tommasovnf92 <91752248+tommasovnf92@users.noreply.github.com>
2024-03-04 22:39:39 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -20,7 +20,7 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc'];
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/2Sbc5kosMTfcf9bXHNteKQhX-1bI02OT','https://rpc.hellomoon.io/1c317de5-f079-4e5f-9268-fc76e643b5d6'];
     const priorityFee = 1; // Priority Fee added to each transaction in Lamports. Set to 0 (zero) to disable priority fees. 1 Lamport = 0.000000001 SOL
 
     const connectionProxy = {
